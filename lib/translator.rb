26285a7099a9e21e730115dfd146f69050a799a9
Donny L <donny.landis@gmail.com>
2020-02-24 17:56:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,12 @@ require "pp"
 def load_library(file)
   emo = YAML.load_file(file)
   translator ={}
-  translator["get_meaning"] = emo
+  eng = 0
+  jap = 1
+  translator["get_meaning"] = {}
+  emo.each_pair do |meaning, emoticon|
+    translator["get_meaning"][emoticon][jap] = meaning
+  end
   translator["get_emoticon"] = {}
   emo.each_pair do |meaning, emoticon|
     translator["get_emoticon"][emoticon] = meaning
