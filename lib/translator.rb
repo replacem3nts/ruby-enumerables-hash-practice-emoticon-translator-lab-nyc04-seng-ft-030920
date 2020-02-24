cb68db981fda41b2394e2fcffab04bef0cd9c472
Donny L <donny.landis@gmail.com>
2020-02-24 18:06:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ end
 
 def get_japanese_emoticon(file, emoticon)
   translator = load_library(file)["get_emoticon"]
-  translator[emoticon]
+  translator[emoticon] ? translator[emoticon] : "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
