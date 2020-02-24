776b8e2f850f2816ed23c3498454b48ef6c701f3
Donny L <donny.landis@gmail.com>
2020-02-24 17:44:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,6 @@ require "pp"
 def load_library(file)
   emo = YAML.load_file(file)
   translator = {"get_meaning": emo}
-  emo.each do |meaning|
-    if !translator["get_meaning": meaning]
-      translator = {"get_meaning": meaning}
-    end
   end
   pp translator
 end
